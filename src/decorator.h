3ed9e69ef6451a5dfa636c55cc08254745cef684
Waqqas Jabbar <waqqas.jabbar@emumba.com>
2019-07-25 17:32:19 +0500

    Added decorate_function() overloads for one and two parameters

--- src/decorator.h
+++ src/decorator.h
@@ -1,6 +1,31 @@
 #pragma once
 
 #include <functional>
+#include <type_traits>
+
+
+template <typename T, typename PRE>
+std::function<T> decorate_function(std::function<T> inner)
+{
+  return [&]() {
+    if (inner)
+      inner();
+  };
+}
+
+
+template <typename T, typename PRE>
+std::function<T> decorate_function(std::function<T> inner, 
+std::function<PRE> pre)
+{
+  return [&]() {
+    if (pre)
+      pre();
+    if (inner)
+      inner();
+  };
+}
+
 
 template <typename T, typename PRE, typename POST>
 std::function<T> decorate_function(std::function<T> inner, std::function<PRE> pre, std::function<POST> post)
